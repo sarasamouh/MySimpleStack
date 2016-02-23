013ed4566d71b1a72f3581c9771921fe8c976a0f
sarasamouh <sarazinebsamouh@gmail.com>
2016-02-23 11:20:35 +0100

    #3 MySimpleStack-1 Exigence3

--- src/main/java/dcll/ssam/Item.java
+++ src/main/java/dcll/ssam/Item.java
@@ -1,7 +1,24 @@
 package dcll.ssam;
 
+import java.util.Objects;
+
 /**
  * Created by mousa on 19/02/2016.
  */
 public class Item {
+
+    private Object value ;
+    Item(Object value){ setValue(value);}
+
+    /**
+     * Exigence nume 3
+     */
+
+    public Object getValue(){
+        return value ;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
 }
